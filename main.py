6d9933108375978a5b136783e55c744da737ef6f
samuel viana <samuelviana2626@gmail.com>
2025-07-07 21:46:03 -0400

    feat(02): adding pre commit with ruff

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print('bem vindo ao esquecido')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    print("bem vindo ao esquecido")
